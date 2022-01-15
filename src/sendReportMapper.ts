072a7c4b306e367235f46fbb4aada2725b51e997
solawat <s.spthx.555@gmail.com>
2022-01-15 13:41:15 +0900

    #1 ユニットテストを追加しました。

---
+++ src/sendReportMapper.ts
@@ -0,0 +1,17 @@
+export module SendReportMapper {
+  export function toMessage(e: any): string {
+    // Googleフォームの項目に合わせる
+    const inquiry_date: string = e.namedValues['タイムスタンプ'];
+    const inquiry_type: string = e.namedValues['お問い合わせ種類'];
+    const inquiry_content: string = e.namedValues['お問い合わせ内容'];
+
+    var message: string = "";
+    message += "[info][title]お問い合わせが来ました[/title]";
+    message += "依頼日時: " + inquiry_date + "\n";
+    message += "お問い合わせ種類: " + inquiry_type + "\n";
+    message += "お問い合わせ内容: " + inquiry_content;
+    message += "[/info]";
+
+    return message;
+  };
+}
